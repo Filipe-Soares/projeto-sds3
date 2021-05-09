b1fd9e6775e52b512270be825c43177cd63e6f4d
filipe.soares <filipe.rocha.soares@gmail.com>
2021-05-09 20:07:47 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -9,19 +10,38 @@ type ChartData = {
 }
 
 const DonutChart = () => {
+  //FORMA ERRADA "não carregará o gráfico"
+  //let chartData: ChartData = { labels: [], series: [] };
 
-  //FORMA ERRADA
-  let chartData: ChartData = { labels: [], series: [] };
-  //FORMA ERRADA
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then((response) => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+  //FORMA ERRADA "entrará em loop infinito ao usar useState"
+  // axios.get(`${BASE_URL}/sales/amount-by-seller`)
+  //   .then((response) => {
+  //     const data = response.data as SaleSum[];
+  //     const myLabels = data.map(x => x.sellerName);
+  //     const mySeries = data.map(x => x.sum);
 
-      chartData = { labels: myLabels, series: mySeries };
-      console.log(chartData)
-    })
+  //     chartData = { labels: myLabels, series: mySeries };
+  //     console.log(chartData)
+  //   })
+
+  //FORMA CORRETA
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+  //FORMA CORRETA
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+      .then((response) => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        setChartData({ labels: myLabels, series: mySeries });
+      });
+  }, []);
+
+
+
+
   // const mockData = {
   //   series: [477138, 499928, 444867, 220426, 473088],
   //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
